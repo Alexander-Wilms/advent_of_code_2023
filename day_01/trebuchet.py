619250ed0dde20e0363151c06c59867d8b23f9d4
Alexander Wilms <f.alexander.wilms@gmail.com>
2023-12-02 02:32:56 +0100

    Day 1: Clean up code

--- day_01/trebuchet.py
+++ day_01/trebuchet.py
@@ -2,62 +2,6 @@ import re
 from pprint import pprint
 from typing import List, Tuple
 
-
-def spelled_out_number_in_line(line: str) -> bool:
-    mapping = {"one": "1", "two": "2", "three": "3", "four": "4", "five": "5", "six": "6", "seven": "7", "eight": "8", "nine": "9"}
-    found_spelled_out_number = 0
-    for key in mapping.keys():
-        if key in line:
-            found_spelled_out_number = 1
-            break
-    return found_spelled_out_number
-
-
-def replace_spelled_out_number(original_line: str) -> str:
-    line = original_line
-    mapping = {"one": "1", "two": "2", "three": "3", "four": "4", "five": "5", "six": "6", "seven": "7", "eight": "8", "nine": "9"}
-    found_spelled_out_numbers = {}
-    attempt = 3
-
-    for key in mapping.keys():
-        # print(f"searching for '{key}' in '{line}'")
-        index = line.find(key)
-        # print(f"index: {index}")
-        if index != -1:
-            found_spelled_out_numbers[index] = key
-    pprint(found_spelled_out_numbers)
-
-    all_found_numbers = found_spelled_out_numbers
-    count = 0
-    for char in line:
-        if re.match(r"\d", char):
-            print(char)
-            all_found_numbers[count] = char
-        count += 1
-
-    pprint(all_found_numbers)
-
-    if attempt == 1:
-        if found_spelled_out_numbers:
-
-            # for key, val in found_spelled_out_numbers.items():
-            first = sorted(list(found_spelled_out_numbers.keys()))[0]
-            first_part = line[0:first]
-            second_part = mapping[found_spelled_out_numbers[first]]
-            third_part = line[first + len(found_spelled_out_numbers[first]) :]
-
-            print(f"{first_part=}")
-            print(f"{second_part=}")
-            print(f"{third_part=}")
-            line = first_part + second_part + third_part
-    elif attempt == 2:
-        if found_spelled_out_numbers:
-            for key, val in mapping.items():
-                line = line.replace(key, val)
-
-    print(f"{original_line} -> {line}")
-    return line
-
 # https://stackoverflow.com/a/4665027/2278742
 def find_all(a_str, sub):
     start = 0
@@ -71,17 +15,18 @@ def find_all(a_str, sub):
 
 def get_digits(line: str):
     mapping = {"one": "1", "two": "2", "three": "3", "four": "4", "five": "5", "six": "6", "seven": "7", "eight": "8", "nine": "9"}
+
+    # search spelled out numbers and save them together with their indices in a dict
     found_spelled_out_numbers = {}
 
     for key in mapping.keys():
-        # print(f"searching for '{key}' in '{line}'")
         indices = list(find_all(line, key))
-        # print(f"index: {index}")
         if indices != -1:
             for index in indices:
                 found_spelled_out_numbers[index] = key
     pprint(found_spelled_out_numbers)
 
+    # search actual digits and save them together with their indices in a dict
     all_found_numbers = found_spelled_out_numbers
     count = 0
     for char in line:
@@ -90,10 +35,7 @@ def get_digits(line: str):
             all_found_numbers[count] = char
         count += 1
 
-    print(f"{all_found_numbers=}")
-    digits = [all_found_numbers[sorted(all_found_numbers.keys())[0]], all_found_numbers[sorted(all_found_numbers.keys())[-1]]]
-    print(f"{digits=}")
-
+    # replace spelled out numbers in dict with actual digits
     for key, val in all_found_numbers.items():
         print(f"checking if {val} in")
         pprint(mapping.keys())
@@ -101,6 +43,8 @@ def get_digits(line: str):
             all_found_numbers[key] = mapping[val]
 
     print(f"{all_found_numbers=}")
+
+    # create list with the two digits that have the lowest and highest index
     digits = [all_found_numbers[sorted(all_found_numbers.keys())[0]], all_found_numbers[sorted(all_found_numbers.keys())[-1]]]
     print(f"{digits=}")
 
