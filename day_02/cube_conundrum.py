74a5255b888baa748f6fce1b118d6f95d01c1311
Alexander Wilms <f.alexander.wilms@gmail.com>
2023-12-02 15:57:00 +0100

    Day 2 part 1 solved

---
+++ day_02/cube_conundrum.py
@@ -0,0 +1,56 @@
+import re
+from pprint import pprint
+
+
+def get_cube_counts(line: str, color: str):
+    matches = re.findall(r"\d* " + color, line)
+    for index in range(0, len(matches)):
+        matches[index] = int(re.findall(r"\d*", matches[index])[0])
+
+    pprint(matches)
+    return matches
+
+
+def solve_puzzle_part(file_name: str, part: int) -> int:
+    games = {}
+
+    cubes_in_bag = {"red": 12, "green": 13, "blue": 14}
+
+    with open(file_name) as f:
+        for line in f.readlines():
+            line = line.strip()
+            print(line)
+            game_id_string = re.findall(r"Game \d*", line)[0]
+            game_id = int(re.findall(r"\d+", game_id_string)[0])
+            print(f"{game_id=}")
+            games[game_id] = {}
+            for color in cubes_in_bag.keys():
+                games[game_id][color] = get_cube_counts(line, color)
+
+    pprint(games)
+
+    sum_of_id_of_games_with_valid_number_of_balls = 0
+
+    for game_id, game_record in games.items():
+        print(f"Checking game {game_id}")
+        possible = True
+        for color in cubes_in_bag.keys():
+            print(f"\tChecking color {color}")
+            if max(games[game_id][color]) <= cubes_in_bag[color]:
+
+                print("\t\tthis number of this color is possible")
+            else:
+                print("\t\tthis number of this color is impossible")
+                possible = False
+        if possible:
+            sum_of_id_of_games_with_valid_number_of_balls += game_id
+
+    print(f"{sum_of_id_of_games_with_valid_number_of_balls=}")
+
+    return sum_of_id_of_games_with_valid_number_of_balls
+
+
+sum = solve_puzzle_part("day_02/example_1.txt", 1)
+assert sum == 8
+sum = solve_puzzle_part("day_02/input.txt", 1)
+assert sum == 2176
