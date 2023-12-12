b408e8cfffb242bc706c45f1811a26fcd5ca2f25
Alexander Wilms <f.alexander.wilms@gmail.com>
2023-12-12 05:58:49 +0100

    Day 11: Part 2 works with efficient algorithm and a Numba example has been added

--- day_11/cosmic_expansion.py
+++ day_11/cosmic_expansion.py
@@ -8,9 +8,14 @@ import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
 import pandas as pd
+from numba import prange
 from pandas import DataFrame
 
 map: Dict[str, Dict[str, str]] = {}
+
+possible_combinations: list = []
+galaxies_expanded: Dict = {}
+G_expanded: nx.Graph = {}
 
 
 def find_empty_cols_and_rows(map: DataFrame) -> tuple[list, list, dict]:
@@ -35,8 +40,8 @@ def find_empty_cols_and_rows(map: DataFrame) -> tuple[list, list, dict]:
             col_count += 1
         row_count += 1
 
-    rows_empty_idx = np.where(rows_empty)[0]
-    cols_empty_idx = np.where(cols_empty)[0]
+    rows_empty_idx = list(np.where(rows_empty)[0])
+    cols_empty_idx = list(np.where(cols_empty)[0])
 
     pprint(f"{rows_empty_idx=}")
     pprint(f"{cols_empty_idx=}")
@@ -45,30 +50,34 @@ def find_empty_cols_and_rows(map: DataFrame) -> tuple[list, list, dict]:
 
 
 def expand_universe(map: DataFrame) -> tuple[DataFrame, list, list]:
+    global factor
+    number_of_copies = factor - 1
     print("Expanding universe")
     (rows, cols) = map.shape
     rows_empty, cols_empty, _ = find_empty_cols_and_rows(map)
 
     empty_col = pd.DataFrame({"V": ["."] * rows})
     for idx in range(len(cols_empty) - 1, 0 - 1, -1):
-        col_idx = cols_empty[idx]
-        print(f"inserting empty col at index {col_idx}")
-        map = pd.concat([map.iloc[:, :col_idx], empty_col, map.iloc[:, col_idx:]], axis=1)
-        print(map)
-        map.columns = range(map.columns.size)
+        for tmp in range(number_of_copies):
+            col_idx = cols_empty[idx]
+            # print(f"inserting empty col at index {col_idx}")
+            map = pd.concat([map.iloc[:, :col_idx], empty_col, map.iloc[:, col_idx:]], axis=1)
+            # print(map)
+            map.columns = range(map.columns.size)
 
     (rows, cols) = map.shape
 
     for idx in range(len(rows_empty) - 1, 0 - 1, -1):
-        row_idx = rows_empty[idx]
-        print(f"inserting empty row at index {row_idx}")
-        # map.loc[row_idx - 0.5] = ["."] * cols
-        empty_row = pd.DataFrame(columns=range(cols))
-        empty_row.loc[0] = ["."] * cols
-        # print(empty_row)
-        map = pd.concat([map.iloc[:row_idx, :], empty_row, map.iloc[row_idx:, :]], axis=0)
-        map.index = range(map.index.size)
-        print(map)
+        for tmp in range(number_of_copies):
+            row_idx = rows_empty[idx]
+            # print(f"inserting empty row at index {row_idx}")
+            # map.loc[row_idx - 0.5] = ["."] * cols
+            empty_row = pd.DataFrame(columns=range(cols))
+            empty_row.loc[0] = ["."] * cols
+            # print(empty_row)
+            map = pd.concat([map.iloc[:row_idx, :], empty_row, map.iloc[row_idx:, :]], axis=0)
+            map.index = range(map.index.size)
+            # print(map)
 
     print("Expanded universe:")
     print(map)
@@ -100,7 +109,22 @@ def map_to_graph(map: DataFrame) -> tuple[nx.Graph, dict, dict]:
     return G, positions, labels
 
 
-def solve_puzzle_part(file_name: str, part: int) -> int:
+# see example on https://numba.pydata.org/
+# but numba decorator doesn't seem to work with Cython
+# TypeError: The decorated object is not a function (got type <class '_cython_3_0_6.cython_function_or_method'>).
+# @njit(parallel=True)
+def numba_example() -> np.ndarray:
+    out = np.arange(1000)
+
+    for i in prange(out.shape[0]):
+        out[i] = 42
+
+    return out
+
+
+def solve_puzzle_part(file_name: str, show_plot: bool, efficient=False) -> int:
+    global G_expanded, galaxies_expanded, possible_combinations
+
     with open(file_name) as f:
         data_single_string = f.read()
 
@@ -124,79 +148,185 @@ def solve_puzzle_part(file_name: str, part: int) -> int:
 
     print(map)
 
-    _, _, galaxies = find_empty_cols_and_rows(map)
+    rows_empty_idx, cols_empty_idx, galaxies = find_empty_cols_and_rows(map)
 
     print(f"{galaxies=}")
 
-    G, positions, labels = map_to_graph(map)
+    if efficient:
+        sum_of_shortest_path_lengths = 0
 
-    map_expanded, _, _ = expand_universe(map)
-    _, _, galaxies_expanded = find_empty_cols_and_rows(map_expanded)
-    pprint(f"{galaxies_expanded=}")
-    G_expanded, positions_expanded, labels_expanded = map_to_graph(map_expanded)
+        for galaxy_id, galaxy_coords in galaxies.items():
+            print(f"{galaxy_id}: {galaxy_coords}")
 
-    if "input.txt" not in file_name:
-        options = {
-            "font_size": 10,
-            "node_size": 1000,
-            "node_color": "white",
-            "edgecolors": "black",
-        }
+            galaxy_row = galaxy_coords[0]
+            galaxy_col = galaxy_coords[1]
 
-        fig, axes = plt.subplots(nrows=1, ncols=2)
-        ax = axes.flatten()
+            empty_rows_so_far = [x for x in rows_empty_idx if x < galaxy_row]
+            empty_cols_so_far = [x for x in cols_empty_idx if x < galaxy_col]
 
-        ax[0].set_title("Original")
-        nx.draw_networkx(G, positions, labels=labels, **options, ax=ax[0])
+            print(f"{empty_rows_so_far=}")
+            print(f"{empty_cols_so_far=}")
 
-        ax[1].set_title("Expanded")
-        nx.draw_networkx(G_expanded, positions_expanded, labels=labels_expanded, **options, ax=ax[1])
+            row_after_expansion = galaxy_row + (factor - 1) * len(empty_rows_so_far)
+            col_after_expansion = galaxy_col + (factor - 1) * len(empty_cols_so_far)
 
-    galaxy_ids = list(galaxies_expanded.keys())
-    possible_combinations = list(combinations(galaxy_ids, 2))
+            print(f"{row_after_expansion=}")
+            print(f"{col_after_expansion=}")
 
-    print(f"In these {number_of_galaxies} galaxies, there are {len(possible_combinations)} pairs.")
+            galaxies[galaxy_id] = [row_after_expansion, col_after_expansion]
 
-    sum_of_shortest_path_lengths = 0
-    combination_count = 1
-    for combination in possible_combinations:
-        # print(f"combination {combination_count}:")
+            print()
 
-        combination_count += 1
-        g1, g2 = combination
+        pprint(f"{galaxies=}")
 
-        g1_row = galaxies_expanded[g1][0]
-        g1_col = galaxies_expanded[g1][1]
+        galaxy_ids = list(galaxies.keys())
+        possible_combinations = list(combinations(galaxy_ids, 2))
 
-        g2_row = galaxies_expanded[g2][0]
-        g2_col = galaxies_expanded[g2][1]
+        print(f"In these {number_of_galaxies} galaxies, there are {len(possible_combinations)} pairs.")
 
-        g1_node_name = f"{g1_row},{g1_col}"
-        g2_node_name = f"{g2_row},{g2_col}"
+        number_of_possible_combinations = len(possible_combinations)
+        combination_count = 0
+        for combination in possible_combinations:
+            print(f"combination {combination_count+1}/{number_of_possible_combinations}")
+            combination_count += 1
 
-        # print(f"\tBetween galaxy {g1} (node '{g1_node_name}') and galaxy {g2} (node '{g2_node_name}')")
+            g1, g2 = combination
 
-        shortest_path = nx.shortest_path(G_expanded, g1_node_name, g2_node_name)
+            g1_row = galaxies[g1][0]
+            g1_col = galaxies[g1][1]
 
-        shortest_path = shortest_path[1:]
-        # print(f"\t{shortest_path=}")
-        shortest_path_len = len(shortest_path)
-        # print(f"\t{shortest_path_len=}")
+            g2_row = galaxies[g2][0]
+            g2_col = galaxies[g2][1]
 
-        sum_of_shortest_path_lengths += shortest_path_len
+            g1_node_name = f"{g1_row},{g1_col}"
+            g2_node_name = f"{g2_row},{g2_col}"
+
+            print(f"\tBetween galaxy {g1} (node '{g1_node_name}') and galaxy {g2} (node '{g2_node_name}')")
+
+            shortest_path_len = max(g2_row, g1_row) - min(g2_row, g1_row) + max(g2_col, g1_col) - min(g2_col, g1_col)
+            print(f"\t{shortest_path_len=}")
+
+            sum_of_shortest_path_lengths += shortest_path_len
+    else:
+        G, positions, labels = map_to_graph(map)
+
+        map_expanded, _, _ = expand_universe(map)
+        _, _, galaxies_expanded = find_empty_cols_and_rows(map_expanded)
+        pprint(f"{galaxies_expanded=}")
+        G_expanded, positions_expanded, labels_expanded = map_to_graph(map_expanded)
+
+        if show_plot:
+            options = {
+                "font_size": 10,
+                "node_size": 1000,
+                "node_color": "white",
+                "edgecolors": "black",
+            }
+
+            fig, axes = plt.subplots(nrows=1, ncols=2)
+            ax = axes.flatten()
+
+            ax[0].set_title("Original")
+            nx.draw_networkx(G, positions, labels=labels, **options, ax=ax[0])
+
+            ax[1].set_title("Expanded")
+            nx.draw_networkx(G_expanded, positions_expanded, labels=labels_expanded, **options, ax=ax[1])
+
+        galaxy_ids = list(galaxies_expanded.keys())
+        possible_combinations = list(combinations(galaxy_ids, 2))
+
+        print(f"In these {number_of_galaxies} galaxies, there are {len(possible_combinations)} pairs.")
+
+        parallel = False
+
+        if parallel:
+            # even running the path searches in parallel might be too slow with 1000 000 inserted cols/rows per empty col/row
+            # there is probably a closed-form expression for the solution
+
+            # iterate through loop in parallel
+            numba_output = numba_example()
+            pprint(numba_output)
+
+            # shortest_path_lengths = find_shortest_path_lengths()
+            # sum_of_shortest_path_lengths = sum(shortest_path_lengths)
+            sum_of_shortest_path_lengths = 0
+        else:
+            sum_of_shortest_path_lengths = 0
+            combination_count = 1
+            number_of_possible_combinations = len(possible_combinations)
+            for combination in possible_combinations:
+                print(f"combination {combination_count}/{number_of_possible_combinations}")
+
+                combination_count += 1
+                g1, g2 = combination
+
+                g1_row = galaxies_expanded[g1][0]
+                g1_col = galaxies_expanded[g1][1]
+
+                g2_row = galaxies_expanded[g2][0]
+                g2_col = galaxies_expanded[g2][1]
+
+                g1_node_name = f"{g1_row},{g1_col}"
+                g2_node_name = f"{g2_row},{g2_col}"
+
+                print(f"\tBetween galaxy {g1} (node '{g1_node_name}') and galaxy {g2} (node '{g2_node_name}')")
+
+                shortest_path = nx.shortest_path(G_expanded, g1_node_name, g2_node_name)
+
+                shortest_path = shortest_path[1:]
+                # print(f"\t{shortest_path=}")
+                shortest_path_len = len(shortest_path)
+                print(f"\t{shortest_path_len=}")
+
+                sum_of_shortest_path_lengths += shortest_path_len
+
+        if show_plot:
+            plt.show(block=True)
 
     print(f"{sum_of_shortest_path_lengths=}")
-    if "input.txt" not in file_name:
-        plt.show(block=True)
+
     return sum_of_shortest_path_lengths
 
 
+factor = 0
+
+
 def test_solutions():
-    steps = solve_puzzle_part("day_11/example_1.txt", 1)
+    global factor
+
+    # inefficient algorithm
+    factor = 2
+    steps = solve_puzzle_part("day_11/example_1.txt", False)
     assert steps == 374
 
-    steps = solve_puzzle_part("day_11/input.txt", 1)
+    factor = 2
+    steps = solve_puzzle_part("day_11/input.txt", False)
     assert steps == 10494813
+
+    factor = 10
+    steps = solve_puzzle_part("day_11/example_1.txt", False)
+    assert steps == 1030
+
+    factor = 100
+    steps = solve_puzzle_part("day_11/example_1.txt", False)
+    assert steps == 8410
+
+    # efficient algorithm
+    factor = 2
+    steps = solve_puzzle_part("day_11/example_1.txt", False, True)
+    assert steps == 374
+
+    factor = 10
+    steps = solve_puzzle_part("day_11/example_1.txt", False, True)
+    assert steps == 1030
+
+    factor = 100
+    steps = solve_puzzle_part("day_11/example_1.txt", False, True)
+    assert steps == 8410
+
+    factor = 1000000
+    steps = solve_puzzle_part("day_11/input.txt", False, True)
+    assert steps == 840988812853
 
 
 if __name__ == "__main__":
